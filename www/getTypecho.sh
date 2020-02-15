96d3103806dc2204f571cd94686adea62c06d0ae
flyisnow <huangzhm@seu.edu.cn>
2020-02-15 23:42:44 +0800

    修改下载脚本

--- www/getTypecho.sh
+++ www/getTypecho.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+cd `dirname $0`
 wget http://typecho.org/downloads/1.1-17.10.30-release.tar.gz
 tar zxvf 1.1-17.10.30-release.tar.gz
+rm 1.1-17.10.30-release.tar.gz
 mv build typecho
 chmod -R 777 typecho
--- www/getWordpress.sh
+++ www/getWordpress.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+cd `dirname $0`
 wget https://downloads.wordpress.org/release/zh_CN/wordpress-5.3.2.zip
 unzip wordpress-5.3.2.zip
+rm wordpress-5.3.2.zip
 mkdir -p wordpress/wp-content/tmp
 chmod -R 777 wordpress
