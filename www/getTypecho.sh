dbb3515461795addf787265b7bc0d3748a4fd1dc
flyisnow <huangzhm@seu.edu.cn>
2020-02-16 11:15:15 +0800

    将typecho版本改为最新版

--- www/getTypecho.sh
+++ www/getTypecho.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 cd `dirname $0`
-wget http://typecho.org/downloads/1.1-17.10.30-release.tar.gz
-tar zxvf 1.1-17.10.30-release.tar.gz
-rm 1.1-17.10.30-release.tar.gz
+#稳定版
+#wget http://typecho.org/downloads/1.1-17.10.30-release.tar.gz
+#tar zxvf 1.1-17.10.30-release.tar.gz
+#rm 1.1-17.10.30-release.tar.gz
+
+#最新版
+wget http://typecho.org/build.tar.gz
+tar zxvf build.tar.gz
+rm build.tar.gz
+
 mv build typecho
 chmod -R 777 typecho
